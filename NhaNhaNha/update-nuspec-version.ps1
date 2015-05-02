54a24e732d980a53fb42b8dffdb2a0fc96f7d2cb
Alberto Monteiro <alberto.monteiro@live.com>
2015-05-02 03:31:58 -0300

    Powershell script to update nuspec version

---
+++ NhaNhaNha/update-nuspec-version.ps1
@@ -0,0 +1,7 @@
+﻿$dllPath = gi ".\bin\Debug\NhaNhaNha.dll";
+$assembly = [System.Reflection.Assembly]::LoadFrom($dllPath);
+$newVersion = $assembly.GetName().Version.ToString();
+[xml]$nuspec = Get-Content .\NhaNhaNha.nuspec;
+$nuspec.package.metadata.version = $newVersion;
+$file = gi ".\NhaNhaNha.nuspec";
+$nuspec.Save($file);
